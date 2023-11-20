eea5b36fe5b9a27bbc4bda2e01c2ec448702d56a
ariecattan <arie.cattan@gmail.com>
2023-11-20 22:55:38 +0200

    port

--- main.py
+++ main.py
@@ -31,4 +31,5 @@ def upload_files():
     return jsonify({"table": df_html})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
